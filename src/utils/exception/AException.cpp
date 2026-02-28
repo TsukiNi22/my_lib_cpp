ff6e5735e8473aa43b2b6acef1636221038c07d7
Tsukini <mat.d.ecole@gmail.com>
2026-02-28 18:59:08 +0100

    feat (OutputEdition): change the write define&macro to inline definition in the namespacce

--- src/utils/exception/AException.cpp
+++ src/utils/exception/AException.cpp
@@ -1,6 +1,6 @@
 /**************************************************************\
 Edition:
-##  @date 26/02/2026 by @author Tsukini
+##  @date 28/02/2026 by @author Tsukini
 
 File Name:
 ##  @file AException.cpp
@@ -13,8 +13,7 @@ File Description:
 #include "utils/exception/ExceptionDefine.hpp"  // utils::exception::Message, utils::exception::Type
 #include "utils/exception/AException.hpp"       // utils::exception::AException
 #include "utils/write/Color.hpp"                // utils::write::Color
-#include "utils/write/WriteDefine.hpp"          // reset, strong
-#include "utils/write/WriteMacro.hpp"           // color_rbg
+#include "utils/write/OutputEdition.hpp"        // utils::write::reset, utils::write::strong, utils::write::color_rbg
 #include "utils/attribute/Attribute.hpp"        // nodiscard
 #include <sstream>                              // std::ostringstream
 #include <string>                               // std::string
@@ -24,22 +23,22 @@ nodiscard std::string utils::exception::AException::formated() const noexcept
     std::ostringstream oss;
 
     // Exception type
-    oss << strong;
+    oss << utils::write::strong();
     if (this->_type & utils::exception::Type::None)
-        oss << color_rgb(175, 100, 0) << "[None]";
+        oss << utils::write::color_rgb(175, 100, 0) << "[None]";
     else if (this->_type & utils::exception::Type::Error)
-        oss << color_rgb(205, 0, 0) << "[Error]";
+        oss << utils::write::color_rgb(205, 0, 0) << "[Error]";
     else if (this->_type & utils::exception::Type::Warning)
-        oss << color_rgb(175, 0, 175) << "[Warning]";
-    oss << " " << reset;
+        oss << utils::write::color_rgb(175, 0, 175) << "[Warning]";
+    oss << " " << utils::write::reset();
 
     // Emplacement information
-    oss << strong << this->_file << ":" << this->_line << reset << " -> " << utils::exception::Message[this->_code] << std::endl;
+    oss << utils::write::strong() << this->_file << ":" << this->_line << utils::write::reset() << " -> " << utils::exception::Message[this->_code] << std::endl;
 
     // Content
-    oss << color_rgb(175, 100, 0) << "-------------------------------------------" << reset << std::endl;
-    oss << color(utils::write::Color::Cyan) << this->_func << color_rgb(175, 100, 0) << " = " << reset << this->_info << std::endl;
-    oss << color_rgb(175, 100, 0) << "-------------------------------------------" << reset;
+    oss << utils::write::color_rgb(175, 100, 0) << "-------------------------------------------" << utils::write::reset() << std::endl;
+    oss << utils::write::color(utils::write::Color::Cyan) << this->_func << utils::write::color_rgb(175, 100, 0) << " = " << utils::write::reset() << this->_info << std::endl;
+    oss << utils::write::color_rgb(175, 100, 0) << "-------------------------------------------" << utils::write::reset();
 
     return oss.str();
 }
