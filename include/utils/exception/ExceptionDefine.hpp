ac36b2d3e9e86f9c94e311e54e66937a8ad0026b
Tsukini <mat.d.ecole@gmail.com>
2026-03-04 17:38:49 +0100

    feat (script): add the auto generation of the exception from the json config file

--- include/utils/exception/ExceptionDefine.hpp
+++ include/utils/exception/ExceptionDefine.hpp
@@ -1,6 +1,6 @@
 /**************************************************************\
 Edition:
-##  @date 03/03/2026 by @author Tsukini
+##  @date 04/03/2026 by @author Tsukini
 
 File Name:
 ##  @file ExceptionDefine.hpp
@@ -16,8 +16,7 @@ File Description:
     /* INCLUDE */
 
     /* type */
-    #include <iterator> // std::size
-    #include <cstddef>  // std::size_t
+    #include "generated_exception_header.hpp"
 
     //----------------------------------------------------------------//
     /* DEFINE */
@@ -41,22 +40,6 @@ enum Type {
     Error   = 0b100,
     Warning = 0b1000,
 };
-
-/* Definition of the different exception code */
-enum Code {
-    Undefined = 0,
-    MouseEvent, // Used in ANSI utils::write::readMouseEvent
-    CODE_SENTINEL // sentinel used for verification
-};
-
-/* Corresponding exception message for each code */
-inline constexpr const char *Message[] = {
-    /* Undefined */ "An undefined error has occured",
-    /* MouseEvent */ "Error during the read of the mouse event",
-};
-
-// Check at the compile time the correspondece between the message & code
-static_assert(std::size(Message) == static_cast<std::size_t>(utils::exception::Code::CODE_SENTINEL), "The message array doesn't correspond to the available exception codes");
 
 } // namespace end
 #endif /* EXCEPTIONDEFINE_H */
